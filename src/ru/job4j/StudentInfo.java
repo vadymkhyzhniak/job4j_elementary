9372b31ef02f4b0d82cfb8c2330ed262fd99c9a8
vadymkhyzhniak <vadymkhyzhniak@gmail.com>
2020-09-17 01:51:52 +0200

    3.3. Разбор первой программы. [#362408]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.sql.SQLOutput;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vadym Khyzhniak");
+        System.out.println("07.12.2000");
+    }
+}
