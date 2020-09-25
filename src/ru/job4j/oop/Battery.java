e237be2815b9197e09aa1f90b8cbbdba46258812
vadymkhyzhniak <vadymkhyzhniak@gmail.com>
2020-09-26 01:13:08 +0200

    1.7. Состояние объекта [#362554]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+}
