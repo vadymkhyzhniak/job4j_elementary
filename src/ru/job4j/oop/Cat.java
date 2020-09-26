7fdc5fd724b7176dcfea5857402c56096439d690
vadymkhyzhniak <vadymkhyzhniak@gmail.com>
2020-09-26 17:07:44 +0200

    1.5. Поля объекта [#362549]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -20,10 +20,12 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("There are gav's food.");
         Cat gav = new Cat();
+        gav.giveNick("Gav");
         gav.eat("kotleta");
         gav.show();
         System.out.println("There are black's food.");
         Cat black = new Cat();
+        black.giveNick("Chorniy");
         black.eat("fish");
         black.show();
     }
